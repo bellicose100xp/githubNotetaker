c0dc382137db67321d46fda981f768b38b464b13
buggy <chandan7@icloud.com>
2015-11-13 09:20:55 -0800

    first commit

---
+++ index.ios.js
@@ -0,0 +1,49 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+import React from 'react-native';
+import Main from './app/components/main';
+
+var {
+    AppRegistry,
+    StyleSheet,
+    NavigatorIOS,
+    Text,
+    View,
+    } = React;
+
+class githubNotetaker extends React.Component {
+    render() {
+        return (
+            <NavigatorIOS
+                initialRoute={{
+                title: 'Github Notetaker',
+                component: Main
+                }}
+            />
+        );
+    }
+}
+
+var styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
+
+AppRegistry.registerComponent('githubNotetaker', () => githubNotetaker);
